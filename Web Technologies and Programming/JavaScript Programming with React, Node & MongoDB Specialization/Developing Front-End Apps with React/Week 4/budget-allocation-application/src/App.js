3804ad22bd6d995706ebb7a3e2c64beedd23d853
Rayen Raddaoui <zero_drive@outlook.com>
2024-02-29 11:16:38 +0100

    lab: Budget Allocation Application

---
+++ Web Technologies and Programming/JavaScript Programming with React, Node & MongoDB Specialization/Developing Front-End Apps with React/Week 4/budget-allocation-application/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Budget from './components/Budget';
+import Remaining from "./components/Remaining";
+import ExpenseTotal from "./components/ExpenseTotal";
+
+import { AppProvider } from './context/AppContext';
+import ExpenseList from "./components/ExpenseList";
+import AllocationForm from "./components/AllocationForm";
+import ExpenseItem from "./components/ExpenseItem";
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <div className='row mt-3'>
+                    {
+                        <div className='col-sm'>
+                            <Budget/>
+                        </div>
+                    }
+
+                    {
+                        <div className='col-sm'>
+                            <Remaining/>
+                        </div>
+                    }
+
+                    {
+                        <div className='col-sm'>
+                            <ExpenseTotal/>
+                        </div>
+                    }
+                    <h3 className='mt-3'>Allocation</h3>
+                    {
+                        <div className='row '>
+                            <div className='col-sm'>
+                                <ExpenseList/>
+                            </div>
+                        </div>
+                    }
+
+                    {
+                        <div className='row '>
+                            <div className='col-sm'>
+                                <ExpenseItem/>
+                            </div>
+                        </div>
+                    }
+                    <h3 className='mt-3'>Change allocation</h3>
+                    {
+
+                        <div className='row mt-3'>
+                            <div className='col-sm'>
+                                <AllocationForm/>
+                            </div>
+                        </div>
+                    }
+
+                </div>
+            </div>
+        </AppProvider>
+    );
+};
+export default App;
