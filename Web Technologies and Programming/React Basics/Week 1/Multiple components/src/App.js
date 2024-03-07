b7163e6af06e2dde84b1274ebbb820cf759f54b3
= <=>
2024-03-07 16:41:25 +0100

    React basics: Multiple components

---
+++ Web Technologies and Programming/React Basics/Week 1/Multiple components/src/App.js
@@ -0,0 +1,13 @@
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card h2="First card's h2" h3="First card's h3"/>
+      <Card h2="Second card's h2" h3="Second card's h3"/>
+      <Card h2="Third card's h2" h3="Third card's h3"/>
+    </div>
+  );
+}
+
+export default App;
