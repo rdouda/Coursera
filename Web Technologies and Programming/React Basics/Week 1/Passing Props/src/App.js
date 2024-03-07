1189d3d07dda7f7330b450d177aafadecd5d631b
= <=>
2024-03-07 16:23:09 +0100

    React basics: Passing props

---
+++ Web Technologies and Programming/React Basics/Week 1/Passing Props/src/App.js
@@ -0,0 +1,11 @@
+import Heading from './components/Heading';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading firstName="rdouda"/>
+    </div>
+  );
+}
+
+export default App;
