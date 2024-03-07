8064a3bd25a684a289b8fe16b5b5be9588971d72
= <=>
2024-03-07 17:06:20 +0100

    React basics: Dynamic events

---
+++ Web Technologies and Programming/React Basics/Week 2/Dynamic events/src/App.js
@@ -0,0 +1,18 @@
+import Card from './components/Card';
+
+function App() {
+  function handleClick(){
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumber);
+    let userInput = prompt('type a number');
+    alert(`Computer number:${randomNumber}, Your guess:${userInput}`);
+  }
+  return (
+    <div className="App">
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Guess the number between 1 and 3</button>
+    </div>
+  );
+}
+
+export default App;
